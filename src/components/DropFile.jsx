7c6d66739298f4421fdd8f9a5c4df02c0aaba1b2
bgsptr <agunggusti95@gmail.com>
2024-09-05 21:36:09 +0800

    successfully create and upload file pdf letter, then try fix btn ui

--- src/components/DropFile.jsx
+++ src/components/DropFile.jsx
@@ -1,101 +1,136 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Dropzone from 'dropzone';
-import 'dropzone/dist/dropzone.css';  // Optional for styling
+import 'dropzone/dist/dropzone.css';
+import { useLetter } from '../hooks/useLetter';
 
-const FileUpload = () => {
-    useEffect(() => {
-        if (!Dropzone.instances.length) {
-        const dropzone = new Dropzone("#my-dropzone", {
-            url: "/upload",
-            acceptedFiles: ".pdf",  // Adjust based on allowed file types
-            maxFilesize: 2,  // Max file size in MB
-            previewsContainer: "[data-hs-file-upload-previews]",
-            clickable: "[data-hs-file-upload-trigger]",
-            previewTemplate: document.querySelector("template[data-hs-file-upload-preview]").innerHTML,
-            init: function () {
-            this.on("uploadprogress", function (file, progress) {
-                const progressBar = file.previewElement.querySelector("[data-hs-file-upload-progress-bar-pane]");
-                const progressValue = file.previewElement.querySelector("[data-hs-file-upload-progress-bar-value]");
-                progressBar.style.width = `${progress}%`;
-                progressValue.textContent = Math.round(progress);
-            });
+/* eslint-disable react/prop-types */
+const FileUpload = (props) => {
+  const { token } = props;
+  const dropzoneRef = useRef(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const { letterData, setLetterData } = useLetter();
+
+  const previewTemplate = `
+    <div class="p-3 bg-white border border-gray-300 border-solid rounded-xl">
+      <div class="flex items-center justify-between mb-1">
+        <div class="flex items-center gap-x-3">
+          <span class="flex items-center justify-center text-gray-500 border border-gray-200 rounded-lg size-10" data-hs-file-upload-file-icon="">
+            <img class="hidden rounded-lg" data-dz-thumbnail="" />
+          </span>
+          <div>
+            <p class="text-sm font-medium text-gray-800">
+              <span class="truncate inline-block max-w-[300px] align-bottom" data-dz-name></span>
+            </p>
+            <p class="text-xs text-gray-500" data-dz-size></p>
+          </div>
+        </div>
+        <div class="flex items-center gap-x-2">
+          <button type="button" class="text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" data-dz-remove>
+            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M3 6h18"></path>
+              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+              <line x1="10" x2="10" y1="11" y2="17"></line>
+              <line x1="14" x2="14" y1="11" y2="17"></line>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="flex items-center gap-x-3 whitespace-nowrap">
+        <div class="flex w-full h-2 overflow-hidden bg-gray-200 rounded-full" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" data-hs-file-upload-progress-bar="">
+          <div class="flex flex-col justify-center overflow-hidden text-xs text-center text-white transition-all duration-500 bg-blue-600 rounded-full whitespace-nowrap hs-file-upload-complete:bg-green-500" style="width: 0;" data-hs-file-upload-progress-bar-pane=""></div>
+        </div>
+        <div class="w-10 text-end">
+          <span class="text-sm text-gray-800">
+            <span data-hs-file-upload-progress-bar-value="">0</span>%
+          </span>
+        </div>
+      </div>
+    </div>
+  `;
+
+  useEffect(() => {
+    if (!Dropzone.instances.length && dropzoneRef.current) {
+      const dropzone = new Dropzone(dropzoneRef.current, {
+        headers:{"Authorization":'Bearer ' + token},  
+        url: "http://localhost:8000/api/upload",
+        acceptedFiles: ".pdf",  // Allowed file type
+        maxFilesize: 10,  // Max file size in MB
+        previewsContainer: "[data-hs-file-upload-previews]",
+        clickable: "[data-hs-file-upload-trigger]",
+        previewTemplate: previewTemplate,
+        init: function () {
+          this.on("uploadprogress", function (file, progress) {
+            const progressBar = file.previewElement.querySelector("[data-hs-file-upload-progress-bar-pane]");
+            const progressValue = file.previewElement.querySelector("[data-hs-file-upload-progress-bar-value]");
+            progressBar.style.width = `${progress}%`;
+            progressValue.textContent = Math.round(progress);
+            console.log('kon')
+          });
+
+          this.on("error", function (file, errorMessage) {
+            if (file.size > this.options.maxFilesize * 1024 * 1024) {
+              setErrorMessage(`Error: File ${file.name} exceeds the size limit of 10MB.`);
+              this.removeFile(file);
             }
-        });
-        }
+          });
 
-        return () => {
-        Dropzone.instances.forEach(dz => dz.destroy());
-        };
-    }, []);
+          this.on("success", function (file, response) {
+            console.log(response);
 
-    return (
-        <div id="my-dropzone" className="hs-file-upload">
-        <template data-hs-file-upload-preview="">
-            <div className="p-3 bg-white border border-gray-300 border-solid rounded-xl">
-            <div className="flex items-center justify-between mb-1">
-                <div className="flex items-center gap-x-3">
-                <span className="flex items-center justify-center text-gray-500 border border-gray-200 rounded-lg size-10" data-hs-file-upload-file-icon="">
-                    <img className="hidden rounded-lg" data-dz-thumbnail="" />
-                </span>
-                <div>
-                    <p className="text-sm font-medium text-gray-800">
-                    <span className="truncate inline-block max-w-[300px] align-bottom" data-hs-file-upload-file-name=""></span>.<span data-hs-file-upload-file-ext=""></span>
-                    </p>
-                    <p className="text-xs text-gray-500" data-hs-file-upload-file-size=""></p>
-                </div>
-                </div>
-                <div className="flex items-center gap-x-2">
-                <button type="button" className="text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" data-hs-file-upload-remove="">
-                    <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M3 6h18"></path>
-                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
-                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
-                    <line x1="10" x2="10" y1="11" y2="17"></line>
-                    <line x1="14" x2="14" y1="11" y2="17"></line>
-                    </svg>
-                </button>
-                </div>
-            </div>
+            const { success, message, file_path } = response;
 
-            <div className="flex items-center gap-x-3 whitespace-nowrap">
-                <div className="flex w-full h-2 overflow-hidden bg-gray-200 rounded-full" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" data-hs-file-upload-progress-bar="">
-                <div className="flex flex-col justify-center overflow-hidden text-xs text-center text-white transition-all duration-500 bg-blue-600 rounded-full whitespace-nowrap hs-file-upload-complete:bg-green-500" style={{ width: '0' }} data-hs-file-upload-progress-bar-pane=""></div>
-                </div>
-                <div className="w-10 text-end">
-                <span className="text-sm text-gray-800">
-                    <span data-hs-file-upload-progress-bar-value="">0</span>%
-                </span>
-                </div>
-            </div>
-            </div>
-        </template>
+            if(success) {
+              setLetterData((prev) => ({
+                ...prev,
+                letter_path: file_path
+              }));
+            }
+          });
+        },
+      });
+    }
 
+    return () => {
+      Dropzone.instances.forEach((dz) => dz.destroy());
+    };
+  }, [previewTemplate]);
+
+  return (
+    <div>
+      <div ref={dropzoneRef} id="my-dropzone" className="hs-file-upload">
         <div className="flex justify-center p-12 bg-white border border-gray-300 border-dashed cursor-pointer rounded-xl" data-hs-file-upload-trigger="">
-            <div className="text-center">
+          <div className="text-center">
             <span className="inline-flex items-center justify-center text-gray-800 bg-gray-100 rounded-full size-16">
-                <svg className="shrink-0 size-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg className="shrink-0 size-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                 <polyline points="17 8 12 3 7 8"></polyline>
                 <line x1="12" x2="12" y1="3" y2="15"></line>
-                </svg>
+              </svg>
             </span>
 
             <div className="flex flex-wrap justify-center mt-4 text-sm leading-6 text-gray-600">
-                <span className="font-medium text-gray-800 pe-1">
+              <span className="font-medium text-gray-800 pe-1">
                 Drop your file here or
-                </span>
-                <span className="font-semibold text-blue-600 bg-white rounded-lg hover:text-blue-700 decoration-2 hover:underline focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2">browse</span>
+              </span>
+              <span className="font-semibold text-blue-600 bg-white rounded-lg hover:text-blue-700 decoration-2 hover:underline focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2">browse</span>
             </div>
 
             <p className="mt-1 text-xs text-gray-400">
-                Pick a file up to 2MB.
+              Pick a file up to 10MB.
             </p>
-            </div>
+          </div>
         </div>
 
         <div className="mt-4 space-y-2 empty:mt-0" data-hs-file-upload-previews=""></div>
-        </div>
-    );
+        {errorMessage && (
+          <div className="mt-2 text-red-500">
+            {errorMessage}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FileUpload;
--- src/components/SubContent.jsx
+++ src/components/SubContent.jsx
@@ -1,69 +1,187 @@
 // import { useAuth } from '../hooks/useAuth';
-import { useLetter } from '../hooks/useLetter';
-import UploadFile from './DropFile';
-import SelectItem from './SelectItem';
-import SelectMultiple from './SelectMultiple';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import { useLetter } from "../hooks/useLetter";
+import UploadFile from "./DropFile";
+import SelectItem from "./SelectItem";
+import SelectMultiple from "./SelectMultiple";
+import Cookies from "js-cookie";
+import { useState } from "react";
 
 export default function SubContent() {
-    // const {  } = useAuth();
-    const { letterData, setLetterData } = useLetter();
+  // const {  } = useAuth();
+  const { letterData, setLetterData } = useLetter();
 
-    const token = Cookies.get('access_token');
+  const [msgSuccess, setMsgSuccess] = useState("");
 
-    const createLetterInput = (e) => {
-        const { name, value } = e.target;
-        setLetterData((prev) => ({
-            ...prev,
-            [name]: value
-        }))
+  const token = Cookies.get("access_token");
+
+  const createLetterInput = (e) => {
+    const { name, value } = e.target;
+    setLetterData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const submitNewLetter = async () => {
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     };
+    const BASE_URL = "http://localhost:8000/api/letter";
+    try {
+      const response = await axios.post(BASE_URL, letterData, options);
+      const { success, message } = response.data;
+      if (success) {
+        setMsgSuccess(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className="col-span-2 p-4 bg-white rounded-lg shadow-md lg:h-full">
-            <h2 className="text-xl font-semibold text-gray-800 dark:text-neutral-200">
-                Fill in the form
-            </h2>
-            <form>
-                <div className="grid gap-4 mt-6 lg:gap-6">
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6">
-                    <div>
-                        <label htmlFor="hs-id-surat" className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">ID Surat</label>
-                        <input type="text" onChange={createLetterInput} name="letter_no" id="hs-id-surat" className="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"/>
-                    </div>
-
-                    <div>
-                        <label htmlFor="hs-tanggal-surat" className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">Tanggal Simpan</label>
-                        <input type="date" name="hs-tanggal-surat" id="hs-tanggal-surat" className="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"/>
-                    </div>
-                    </div>
-
-                    <div>
-                        <label htmlFor="hs-judul-surat" className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">Judul</label>
-                        <input type="text" onChange={createLetterInput} name="letter_title" id="hs-judul-surat" autoComplete="email" className="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"/>
-                    </div>
-
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6">
-                        <div>
-                            <label htmlFor="hs-tipe-surat" className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">Tipe Surat</label>
-                            <SelectItem 
-                            name = "hs-tipe-surat" token = {token}
-                            />
-                        </div>
-
-                        <div>
-                            <label htmlFor="hs-keyword-surat" className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">List Keyword</label>
-                            <SelectMultiple 
-                            name = "hs-list-keyword" token = {token}
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="hs-about-hire-us-1" className="block mb-2 text-sm font-medium text-gray-700 dark:text-white">File Surat</label>
-                        <UploadFile />
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="col-span-2 p-4 bg-white rounded-lg shadow-md lg:h-full">
+      {msgSuccess && (
+        <div
+          className="bg-teal-50 border-t-2 border-teal-500 rounded-lg p-4 dark:bg-teal-800/30"
+          role="alert"
+          tabIndex="-1"
+          aria-labelledby="hs-bordered-success-style-label"
+        >
+          <div className="flex">
+            <div className="shrink-0">
+              <span className="inline-flex justify-center items-center size-8 rounded-full border-4 border-teal-100 bg-teal-200 text-teal-800 dark:border-teal-900 dark:bg-teal-800 dark:text-teal-400">
+                <svg
+                  className="shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path>
+                  <path d="m9 12 2 2 4-4"></path>
+                </svg>
+              </span>
+            </div>
+            <div className="ms-3">
+              <h3
+                id="hs-bordered-success-style-label"
+                className="text-gray-800 font-semibold dark:text-white"
+              >
+                {msgSuccess}.
+              </h3>
+              <p className="text-sm text-gray-700 dark:text-neutral-400">
+                You have successfully create a letter.
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      )}
+
+      <h2 className="text-xl font-semibold text-gray-800 dark:text-neutral-200">
+        Fill in the form
+      </h2>
+      <form>
+        <div className="grid gap-4 mt-6 lg:gap-6">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6">
+            <div>
+              <label
+                htmlFor="hs-id-surat"
+                className="block mb-2 text-sm font-medium text-gray-700 dark:text-white"
+              >
+                ID Surat
+              </label>
+              <input
+                type="text"
+                onChange={createLetterInput}
+                name="letter_no"
+                id="hs-id-surat"
+                className="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="hs-tanggal-surat"
+                className="block mb-2 text-sm font-medium text-gray-700 dark:text-white"
+              >
+                Tanggal Simpan
+              </label>
+              <input
+                type="date"
+                name="hs-tanggal-surat"
+                id="hs-tanggal-surat"
+                className="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="hs-judul-surat"
+              className="block mb-2 text-sm font-medium text-gray-700 dark:text-white"
+            >
+              Judul
+            </label>
+            <input
+              type="text"
+              onChange={createLetterInput}
+              name="letter_title"
+              id="hs-judul-surat"
+              autoComplete="email"
+              className="block w-full px-4 py-3 text-sm border border-gray-200 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            />
+          </div>
+
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6">
+            <div>
+              <label
+                htmlFor="hs-tipe-surat"
+                className="block mb-2 text-sm font-medium text-gray-700 dark:text-white"
+              >
+                Tipe Surat
+              </label>
+              <SelectItem name="hs-tipe-surat" token={token} />
+            </div>
+
+            <div>
+              <label
+                htmlFor="hs-keyword-surat"
+                className="block mb-2 text-sm font-medium text-gray-700 dark:text-white"
+              >
+                List Keyword
+              </label>
+              <SelectMultiple name="hs-list-keyword" token={token} />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="hs-about-hire-us-1"
+              className="block mb-2 text-sm font-medium text-gray-700 dark:text-white"
+            >
+              File Surat
+            </label>
+            <UploadFile token={token} />
+          </div>
+          <div>
+            <button
+              type="button"
+              className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+              onClick={submitNewLetter}
+            >
+              Create Letter
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
