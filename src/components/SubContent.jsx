b3e83c27ee3e77b297fd2ee293c372cd593c2ded
bgsptr <agunggusti95@gmail.com>
2024-09-09 23:22:02 +0800

    prev and next button already worked using disabled attribute

--- src/components/SubContent.jsx
+++ src/components/SubContent.jsx
@@ -538,6 +538,7 @@ export default function SubContent({ activeMenu }) {
   // const [viewLetter, setViewLetter] = useState([]);
 
   const [token, setToken] = useState(Cookies.get("access_token"));
+  const [disableNext, setDisableNext] = useState(false);
 
 
   // useEffect(() => {
@@ -561,6 +562,11 @@ export default function SubContent({ activeMenu }) {
       const letter = response.data;
       console.log(letter.data);
       setLetterData(letter.data);
+      if (letter.data.length < 10) {
+        setDisableNext(true);
+      } else {
+        setDisableNext(false);
+      }
       setLoading(true)
     } catch(err) {
       console.log(err);
@@ -913,7 +919,7 @@ export default function SubContent({ activeMenu }) {
                           </div> */}
                           <nav className="flex items-center gap-x-1" aria-label="Pagination">
                             {/* <button type="button" onClick={(e) => setPage(page - 1)} className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Previous" disabled="true"> */}
-                            <button type="button" onClick={(e) => setPage(page - 1)} className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Previous">
+                            <button type="button" onClick={(e) => setPage(page - 1)} className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Previous" disabled={page === 0 ? true : false}>
                               <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="m15 18-6-6 6-6"></path>
                               </svg>
@@ -924,7 +930,7 @@ export default function SubContent({ activeMenu }) {
                               <button type="button" className="min-h-[38px] min-w-[38px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">2</button>
                               <button type="button" className="min-h-[38px] min-w-[38px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">3</button>
                             </div>
-                            <button type="button" onClick={(e) => setPage(page + 1)} className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Next">
+                            <button type="button" onClick={(e) => setPage(page + 1)} className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Next" disabled={disableNext}>
                               <span>Next</span>
                               <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="m9 18 6-6-6-6"></path>
